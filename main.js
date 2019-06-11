c0cf94ac74e26201045406cf8c41c14f0082d40d
Aleksey <aleksey.bestuzhev@gmail.com>
2019-06-11 12:48:02 +0300

    changes

--- main.js
+++ main.js
@@ -14,4 +14,32 @@ $(function(){
         // $(this).parents('.tabs-menu').parent().siblings('.tab').find(".tab-content").not(tab).css("display", "none");
         $(tab).fadeIn();
     });
+
+    $('.card-slider-big').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        dots: false,
+        asNavFor: '.card-slider-small',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                arrows: true,
+                dots: true
+              }
+            },
+            
+          ]
+    });
+
+    $('.card-slider-small').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.card-slider-big',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true
+    });
 });
